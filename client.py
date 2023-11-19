ef38b652ed0496238e3662be8f403d8aed321b4c
Earthysteve357 <124849580+Earthysteve357@users.noreply.github.com>
2023-11-19 11:59:35 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import keyboard
+import random
+import select
+import PySimpleGUI as sg
+
+text = ''
+
+layout = [[sg.Multiline(default_text=text,size=(50,20),disabled=True,key='multiline')],
+          [sg.InputText(key='msg'),sg.Button('Send')]]
+window = sg.Window('Chatroom',layout)
+
+hostname = 'localhost'
+port = 16556
+
+s = socket.socket()
+s.connect((hostname,port))
+
+while True:
+    ready_to_read, ready_to_write, error = select.select([s],[],[],0.1)
+    if not ready_to_read == []:
+        msg = s.recv(1024).decode()
+        print(msg)
+        text = text + msg + '\n'
+        window['multiline'].update(text)
+    event,values = window.read()
+    if event == sg.WIN_CLOSED:
+        window.close()
+        s.close()
+        break
+    if event == 'Send':
+        msg = values['msg']
+        window['msg'].update('')
+        text = text + '<you>' + msg + '\n'
+        window['multiline'].update(text)
+        s.sendall(msg.encode())
---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+import select
+import keyboard
+import random
+
+hostname = 'localhost'
+port = 16556
+
+clients = []
+
+s = socket.socket()
+s.bind((hostname,port))
+s.listen()
+
+
+def broadcast(msg,addr):
+    msg = f'<{addr[0]}>' + msg.decode()
+    msg = msg.encode()
+    for client in clients:
+        if client[1] != None: #replace None with addr to stop msg from getting sent to sender
+            client[0].sendall(msg)
+
+while True:
+    client_list = []
+    for client in clients:
+        client_list.append(client[0])
+    ready_to_read, ready_to_write, error = select.select([s,*client_list],[],[],0.1)
+    for sock in ready_to_read:
+        if sock == s:
+            conn, addr = s.accept()
+            clients.append((conn,addr))
+            print(addr[0] + ' connected')
+        else:
+            try:
+                msg = sock.recv(1024)
+                broadcast(msg,sock.getsockname())
+            except ConnectionResetError:
+                clients.remove((sock,sock.getsockname()))
+                print(clients)
