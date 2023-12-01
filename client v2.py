8cdc1a359c05cb7843e5f0ac0d35e93f7ec177cb
noah <noahmankoff@gmail.com>
2023-11-30 21:35:03 -0500

    Surface

--- client v2.py
+++ client v2.py
@@ -4,31 +4,27 @@ import keyboard
 import PySimpleGUI as sg
 
 def receive_msg():
-    global text
     while True:
         msg = s.recv(1024).decode()
-        text = text + msg + '\n'
-        window['multiline'].update(text)
+        window['multiline'].print(msg)
 
 def send(msg):
+    window['msg'].update('')
+    msg = msg.strip()
     if msg == '':
         return
-    global text
-    window['msg'].update('')
-    text = text + f'<you>{msg}\n'
-    window['multiline'].update(text)
+    window['multiline'].print(f'<you>{msg}',justification='right')
     print(f'sending {msg}')
     s.sendall(f'<{display_name}>{msg}'.encode())
 
 
-text = ''
-layout = [[sg.Multiline(default_text=text,size=(50,20),disabled=True,key='multiline')],
+layout = [[sg.Multiline(size=(50,20),disabled=True,autoscroll=True,key='multiline')],
           [sg.InputText(key='msg'),sg.Button('Send')],
           [sg.InputText(size=(31,1),key='name'),sg.Button('Change Display Name')]]
 window = sg.Window('Chatroom',layout,finalize=True)
 
 
-hostname = 'localhost'
+hostname = socket.gethostbyname(socket.gethostname())
 port = 16556
 
 s = socket.socket()
@@ -37,12 +33,12 @@ s.connect((hostname,port))
 display_name = s.getsockname()[1]
 window['name'].update(s.getsockname()[1])
 
+
 s_thread = threading.Thread(target=receive_msg,daemon=True)
 s_thread.start()
 
 while True:
-    print('looping')
-    event,values = window.read(timeout=500)
+    event,values = window.read(timeout=10)
     if event == sg.WIN_CLOSED:
         window.close()
         s.close()
@@ -50,7 +46,11 @@ while True:
     if event == 'Send':
         send(values['msg'])
     if event == 'Change Display Name':
-        if not len(values['name']) > 15:
+        if len(values['name']) > 15:
+            window['name'].update(display_name)
+        else:
             display_name = values['name']
-    # if keyboard.read_key() == 'enter':
-    #     send(values['msg'])
+    if keyboard.is_pressed('enter'):
+        while keyboard.is_pressed('enter'):
+            pass
+        send(values['msg'])
