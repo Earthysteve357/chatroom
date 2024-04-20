69170c242e66a7f3222b06bbc974e2738fc13c44
noahm <noahmankoff@gmail.com>
2024-04-20 19:29:52 -0400

    Updates to window.joinrm.menu

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from tkinter import END
 import customtkinter as ctk
 import socket
 import threading
@@ -9,6 +10,7 @@ class Window:
         self.root.wm_title('Chatroom')
         self.inrm_frame = Frame(self.root)
         self.inrm_frame.textbox = ctk.CTkTextbox(master=self.inrm_frame.frame,state='disabled')
+        self.inrm_frame.textbox.tag_config('right',justify='right')
         self.inrm_frame.textbox.pack(fill='both',expand=True)
         self.inrm_frame.message = ctk.CTkEntry(master=self.inrm_frame.frame)
         self.inrm_frame.message.pack(fill='x')
@@ -20,14 +22,17 @@ class Window:
         self.newrm_frame.name = ctk.CTkEntry(master=self.newrm_frame.frame)
         self.newrm_frame.name.grid(row=1,column=1)
         self.newrm_frame.text2 = ctk.CTkLabel(master=self.newrm_frame.frame,text='Password: ').grid(row=2,column=0)
-        self.newrm_frame.password = ctk.CTkEntry(master=self.newrm_frame.frame,)
+        self.newrm_frame.password = ctk.CTkEntry(master=self.newrm_frame.frame,show='*')
         self.newrm_frame.password.grid(row=2,column=1)
         self.joinrm_frame = Frame(self.root)
         self.joinrm_frame.button = ctk.CTkButton(master=self.joinrm_frame.frame,text='Join Room').grid(row=0,column=0)
-        self.joinrm_frame.menu = ctk.CTkOptionMenu(master=self.joinrm_frame.frame, values=server.rooms)
+        if server.rooms == '[0]':
+            self.joinrm_frame.menu = ctk.CTkOptionMenu(master=self.joinrm_frame.frame, values=['No Rooms'],state='disabled')
+        else:
+            self.joinrm_frame.menu = ctk.CTkOptionMenu(master=self.joinrm_frame.frame, values=server.rooms)
         self.joinrm_frame.menu.grid(row=0,column=1)
         self.joinrm_frame.text1 = ctk.CTkLabel(master=self.joinrm_frame.frame,text='Password:').grid(row=1,column=0)
-        self.joinrm_frame.password = ctk.CTkEntry(master=self.joinrm_frame.frame)
+        self.joinrm_frame.password = ctk.CTkEntry(master=self.joinrm_frame.frame,show='*')
         self.joinrm_frame.password.grid(row=1,column=1)
         self.joinrm_frame.frame.pack()
         self.newrm_frame.frame.pack(pady=20)
@@ -43,12 +48,15 @@ class Connection:
         self.createrm_thread = threading.Thread(target=self.createrm)
         self.rooms = self.server.recv(1024)
         self.rooms = (self.rooms).decode()
-        self.rooms = self.rooms.split(',')
-        x = 0
-        for room in self.rooms:
-            room = room.strip()
-            self.rooms[x] = room
-            x += 1
+        if self.rooms == '[0]':
+            pass
+        else:
+            self.rooms = self.rooms.split(',')
+            x = 0
+            for room in self.rooms:
+                room = room.strip()
+                self.rooms[x] = room
+                x += 1
     def createrm(self):
         name = window.newrm_frame.name.get()
         name = name.strip()
@@ -72,8 +80,12 @@ class Connection:
                 window.inrm_frame.frame.pack(fill='both',expand=True)
     def send(self):
         msg = window.inrm_frame.message.get()
-        window.inrm_frame.message.delete(0,'end')
-        print(msg)
+        msg = msg.strip()
+        window.inrm_frame.message.delete(0,END)
+        window.inrm_frame.textbox.configure(state='normal')
+        window.inrm_frame.textbox.insert(END,'<you>'+msg+'\n','right')
+        window.inrm_frame.textbox.configure(state='disabled')
+        self.server.sendall(msg.encode())
     def listen(self):
         while True:
             data = self.server.recv(1024)
